8c1ebf22bcd6190fdf8482c3916c3dc9dafdbf23
Oliver Schmidt <oliverlucaschmidt@gmail.com>
2023-11-06 13:41:17 +0100

    Nach einer Zeit x wird abgeschaltete

--- hue_1.py
+++ hue_1.py
@@ -30,11 +30,11 @@ logging.basicConfig(
 # Data structure to track lamp state and time last turned on
 def initialize_lamp_states(zonen_states):
     lamp_states = {
-        item["lamp"]: {"on": False,
-                       "last_turned_on_time": None, "brightness": None}
+        item["lamp"]: {"on": False, "last_turned_on_time": None, "brightness": None, "t_x": item.get("t_x", 0)}
         for item in zonen_states
     }
     return lamp_states
+
 
 
 def open_json(pfad):
@@ -42,32 +42,33 @@ def open_json(pfad):
         daten = json.load(datei)
     return daten
 
+
 # parameters: 'on' : True|False , 'bri' : 0-254, 'sat' : 0-254, 'ct': 154-500
 def turn_on_lights(lights, bri, sat, ct):
-    """ Adjust properties of one or more lights.
+    """Adjust properties of one or more lights.
 
-        light_id can be a single lamp or an array of lamps
-        parameters: 'bri' : 0-254, 'sat' : 0-254, 'ct': 154-500
+    light_id can be a single lamp or an array of lamps
+    parameters: 'bri' : 0-254, 'sat' : 0-254, 'ct': 154-500
 
-        """
+    """
     for item in lights:
         lamp = item["lamp"]
-        b.set_light(lamp, 'on', True)
+        b.set_light(lamp, "on", True)
         if bri is not None:
             print(lamp, "1")
-            b.set_light(lamp, 'bri', bri)
+            b.set_light(lamp, "bri", bri)
         if sat is not None:
             print(lamp, "2")
-            b.set_light(lamp, 'sat', sat)
+            b.set_light(lamp, "sat", sat)
         if ct is not None:
             print(lamp, "3")
-            b.set_light(lamp, 'ct', ct)
-    
+            b.set_light(lamp, "ct", ct)
+
 
 def turn_off_lights(lights):
     for item in lights:
         lamp = item["lamp"]
-        b.set_light(lamp, 'on', False)
+        b.set_light(lamp, "on", False)
 
 
 def sunrise():
@@ -120,14 +121,14 @@ def sunset():
     return formatted_sunset_time
 
 
-def check_lamp_state(lamp_array):
-    current_datetime = datetime.datetime.now()
-    lamp_states = initialize_lamp_states(lamp_array)
 
+def check_lamp_state(lamp_array, lamp_states):
+    current_datetime = datetime.datetime.now()
     true_lamps = []
+
     for item in lamp_array:
         lamp = item["lamp"]
-        on_state = b.get_light(lamp, "on")
+        on_state = b.get_light(lamp, 'on')
 
         if on_state:
             true_lamps.append(lamp)
@@ -137,13 +138,25 @@ def check_lamp_state(lamp_array):
             if lamp_states[lamp]["last_turned_on_time"] is None:
                 lamp_states[lamp]["last_turned_on_time"] = current_datetime
 
+    for lamp in true_lamps:
+        last_turned_on_time = lamp_states[lamp]["last_turned_on_time"]
+        t_x = lamp_states[lamp]["t_x"]
+
+        if (current_datetime - last_turned_on_time).total_seconds() > t_x:
+            print("Ich bin hier")
+            b.set_light(lamp, 'on', False)
+            lamp_states[lamp]["on"] = False
+            lamp_states[lamp]["last_turned_on_time"] = None
+
     return true_lamps
+
+
 
 
 def main_function():
     zonen_json = open_json(Pfad_json)
     logging.info("\nRestart")
-
+    lamp_states = initialize_lamp_states(zonen_json["Olli"])
     Morning = 0
     Day = 0
     Afternoon = 0
@@ -157,15 +170,15 @@ def main_function():
         sunrise_time = datetime.datetime.strptime(formatted_sunrise_time, "%H:%M:%S").time()
         sunrise_time_deltatime = datetime.datetime.combine(datetime.datetime.today(), sunrise_time) - deltatime
         sunrise_time_deltatime = sunrise_time_deltatime.time()
-        
+
         sunset_time = datetime.datetime.strptime(formatted_sunset_time, "%H:%M:%S").time()
         sunset_time_deltatime1 = datetime.datetime.combine(datetime.datetime.today(), sunset_time) + deltatime
         sunset_time_deltatime = sunset_time_deltatime.time()
         """
-        #turn_on_lights(zonen_json["Olli"], 250, 100, 500)
+        # turn_on_lights(zonen_json["Olli"], 250, 100, 500)
+        
+        true_lamps = check_lamp_state(zonen_json["Olli"], lamp_states)
 
-
-        true_lamps = check_lamp_state(zonen_json["Olli"])
         time.sleep(2)
         print("True Lamps:", true_lamps)
 
