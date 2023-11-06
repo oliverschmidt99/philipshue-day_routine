480a3cc9d5c8912a795b6fae3ade3839c7d86017
Oliver Schmidt <oliverlucaschmidt@gmail.com>
2023-11-06 17:06:19 +0100

    Funktion coming_home fehlerbehaftet

--- hue_1.py
+++ hue_1.py
@@ -30,11 +30,15 @@ logging.basicConfig(
 # Data structure to track lamp state and time last turned on
 def initialize_lamp_states(zonen_states):
     lamp_states = {
-        item["lamp"]: {"on": False, "last_turned_on_time": None, "brightness": None, "t_x": item.get("t_x", 0)}
+        item["lamp"]: {
+            "on": False,
+            "last_turned_on_time": None,
+            "brightness": None,
+            "t_x": item.get("t_x", 0),
+        }
         for item in zonen_states
     }
     return lamp_states
-
 
 
 def open_json(pfad):
@@ -55,13 +59,10 @@ def turn_on_lights(lights, bri, sat, ct):
         lamp = item["lamp"]
         b.set_light(lamp, "on", True)
         if bri is not None:
-            print(lamp, "1")
             b.set_light(lamp, "bri", bri)
         if sat is not None:
-            print(lamp, "2")
             b.set_light(lamp, "sat", sat)
         if ct is not None:
-            print(lamp, "3")
             b.set_light(lamp, "ct", ct)
 
 
@@ -121,14 +122,13 @@ def sunset():
     return formatted_sunset_time
 
 
-
 def check_lamp_state(lamp_array, lamp_states):
     current_datetime = datetime.datetime.now()
     true_lamps = []
 
     for item in lamp_array:
         lamp = item["lamp"]
-        on_state = b.get_light(lamp, 'on')
+        on_state = b.get_light(lamp, "on")
 
         if on_state:
             true_lamps.append(lamp)
@@ -143,44 +143,119 @@ def check_lamp_state(lamp_array, lamp_states):
         t_x = lamp_states[lamp]["t_x"]
 
         if (current_datetime - last_turned_on_time).total_seconds() > t_x:
-            print("Ich bin hier")
-            b.set_light(lamp, 'on', False)
+            logging.info("Mode - Turn_off_light", lamp)
+            b.set_light(lamp, "on", False)
             lamp_states[lamp]["on"] = False
             lamp_states[lamp]["last_turned_on_time"] = None
 
-    return true_lamps
+
+def coming_home(lamp_states):
+    current_datetime = datetime.datetime.now()
+    end_time = current_datetime + timedelta(seconds=18)
+
+    for light_name, light_data in zonen_json["Dummy"]:
+        on = b.get_light(light_name, "on")
+    b.set_light(zonen_json["Dummy"], "on", False)
+
+    if on:
+        logging.info("Coming Home - Mode")
+
+        while datetime.now() < end_time:
+            # Turn on lights in zone_runway
+            for light in zonen_json["zone_runway"]:
+                light_names[light].on = True
+                light_names[light].brightness = 250
+                time.sleep(1)
+
+        # After the delay, turn on lights in zone_cominghome
+        turn_on_lights(zonen_json["zone_runway"], 180, None, None)
+
+        # Calculate the minimum t_x value among all lamps
+        min_t_x = min(lamp_states[lamp]["t_x"] for lamp in lamp_states)
+
+        while datetime.now() - end_time < timedelta(seconds=min_t_x):
+            for lamp in lamp_states:
+                last_turned_on_time = lamp_states[lamp]["last_turned_on_time"]
+                t_x = lamp_states[lamp]["t_x"]
+
+                if (current_datetime - last_turned_on_time).total_seconds() > t_x:
+                    logging.info("Mode - Turn_off_light", lamp)
+                    b.set_light(lamp, "on", False)
+                    lamp_states[lamp]["on"] = False
+                    lamp_states[lamp]["last_turned_on_time"] = None
 
 
+zonen_json = open_json(Pfad_json)
 
 
 def main_function():
-    zonen_json = open_json(Pfad_json)
-    logging.info("\nRestart")
-    lamp_states = initialize_lamp_states(zonen_json["Olli"])
+    logging.info("Restart\n")
+
+    lamp_states_Olli = initialize_lamp_states(zonen_json["Olli"])
+    lamp_states_zone_daymode = initialize_lamp_states(zonen_json["zone_daymode"])
+    lamp_states_dummy = initialize_lamp_states(zonen_json["Dummy"])
+    lamp_states_zone_cominghome = initialize_lamp_states(zonen_json["zone_cominghome"])
+
     Morning = 0
     Day = 0
-    Afternoon = 0
+    Evening = 0
     Night = 0
     while True:
-        """
         formatted_sunrise_time = sunrise()
         formatted_sunset_time = sunset()
         deltatime = datetime.timedelta(hours=1)
 
-        sunrise_time = datetime.datetime.strptime(formatted_sunrise_time, "%H:%M:%S").time()
-        sunrise_time_deltatime = datetime.datetime.combine(datetime.datetime.today(), sunrise_time) - deltatime
+        sunrise_time = datetime.datetime.strptime(
+            formatted_sunrise_time, "%H:%M:%S"
+        ).time()
+        sunrise_time_deltatime = (
+            datetime.datetime.combine(datetime.datetime.today(), sunrise_time)
+            - deltatime
+        )
         sunrise_time_deltatime = sunrise_time_deltatime.time()
 
-        sunset_time = datetime.datetime.strptime(formatted_sunset_time, "%H:%M:%S").time()
-        sunset_time_deltatime1 = datetime.datetime.combine(datetime.datetime.today(), sunset_time) + deltatime
+        sunset_time = datetime.datetime.strptime(
+            formatted_sunset_time, "%H:%M:%S"
+        ).time()
+        sunset_time_deltatime = (
+            datetime.datetime.combine(datetime.datetime.today(), sunset_time)
+            + deltatime
+        )
         sunset_time_deltatime = sunset_time_deltatime.time()
-        """
-        # turn_on_lights(zonen_json["Olli"], 250, 100, 500)
-        
-        true_lamps = check_lamp_state(zonen_json["Olli"], lamp_states)
 
-        time.sleep(2)
-        print("True Lamps:", true_lamps)
+        if sunrise_time < datetime.datetime.now().time() < sunset_time:
+            if Day != 1:
+                Day += 1
+                Night = 0
+                logging.info("Mode - Day")
+            check_lamp_state(zonen_json["zone_daymode"], lamp_states_zone_daymode)
+
+        else:
+            if sunset_time <= datetime.datetime.now().time() < sunset_time_deltatime:
+                if Evening != 1:
+                    Evening += 1
+                    Day = 0
+                    logging.info("Mode - Evening")
+                turn_on_lights(zonen_json["outside"], 150, None, None)
+                turn_on_lights(zonen_json["zone_waylight"], 150, None, None)
+                # coming_home(lamp_states_dummy)
+
+            elif (
+                sunrise_time_deltatime < datetime.datetime.now().time() <= sunrise_time
+            ):
+                if Morning != 1:
+                    Morning += 1
+                    Evening = 0
+                    logging.info("Mode - Morning")
+                turn_on_lights(zonen_json["outside"], 150, None, None)
+                turn_on_lights(zonen_json["zone_waylight"], 150, None, None)
+            else:
+                if Night != 1:
+                    Night += 1
+                    Morning = 0
+                    logging.info("Mode - Night")
+                check_lamp_state(zonen_json["zone_daymode"], lamp_states_zone_daymode)
+                # coming_home(lamp_states_dummy)
 
 
 if __name__ == "__main__":
--- outside.py
+++ outside.py
@@ -18,11 +18,11 @@ lights = b.lights
 
 Dummy = ["Elena/Deckelampe"]
 Olli = [
-    {"lamp": "Olli/Sony TV/Links/Hue Play", "t_x":  120},
+    {"lamp": "Olli/Sony TV/Links/Hue Play", "t_x": 120},
     {"lamp": "Olli/Sony TV/Rechts/Hue Play", "t_x": 120},
-    {"lamp": "Olli/Regal/Hue Go", "t_x":            120},
+    {"lamp": "Olli/Regal/Hue Go", "t_x": 120},
     {"lamp": "Olli/Schreibtisch/Hue Go", "t_x": 120},
-    {"lamp": "Olli/Decke 1/Hue color", "t_x": 120}
+    {"lamp": "Olli/Decke 1/Hue color", "t_x": 120},
 ]
 outside = [
     {"lamp": "Draußen/Terrasse/Tür/Ost", "t_x": 120},
@@ -38,7 +38,7 @@ outside = [
     {"lamp": "Draußen/Auffahrt/Innen/1", "t_x": 120},
     {"lamp": "Draußen/Auffahrt/Innen/2", "t_x": 120},
     {"lamp": "Draußen/Auffahrt/Innen/3", "t_x": 120},
-    {"lamp": "Draußen/Auffahrt/Innen/4", "t_x": 120}
+    {"lamp": "Draußen/Auffahrt/Innen/4", "t_x": 120},
 ]
 zone_daymode = [
     {"lamp": "Draußen/Terrasse/Tür/Ost", "t_x": 120},
@@ -58,7 +58,7 @@ zone_daymode = [
     {"lamp": "Draußen/Auffahrt/Innen/1", "t_x": 120},
     {"lamp": "Draußen/Auffahrt/Innen/2", "t_x": 120},
     {"lamp": "Draußen/Auffahrt/Innen/3", "t_x": 120},
-    {"lamp": "Draußen/Auffahrt/Innen/4", "t_x": 120}
+    {"lamp": "Draußen/Auffahrt/Innen/4", "t_x": 120},
 ]
 zone_outside = [
     {"lamp": "Draußen/Terrasse/Tür/Ost", "t_x": 120},
@@ -68,7 +68,7 @@ zone_outside = [
     {"lamp": "Draußen/Küche/R/Nord", "t_x": 120},
     {"lamp": "Draußen/Haustür/West", "t_x": 120},
     {"lamp": "Draußen/Carport/Vorne 1/", "t_x": 120},
-    {"lamp": "Draußen/Carport/Vorne 2/", "t_x": 120}
+    {"lamp": "Draußen/Carport/Vorne 2/", "t_x": 120},
 ]
 zone_runway = [
     {"lamp": "Draußen/Auffahrt/Innen/1", "t_x": 120},
@@ -78,7 +78,7 @@ zone_runway = [
     {"lamp": "Draußen/Auffahrt/Außen/2", "t_x": 120},
     {"lamp": "Draußen/Auffahrt/Innen/4", "t_x": 120},
     {"lamp": "Draußen/Carport/Vorne 1/", "t_x": 120},
-    {"lamp": "Draußen/Carport/Vorne 2/", "t_x": 120}
+    {"lamp": "Draußen/Carport/Vorne 2/", "t_x": 120},
 ]
 zone_cominghome = [
     {"lamp": "Draußen/Terrasse/Tür/Ost", "t_x": 120},
@@ -90,32 +90,43 @@ zone_cominghome = [
     {"lamp": "Draußen/Carport/Vorne 1/", "t_x": 120},
     {"lamp": "Draußen/Carport/Vorne 2/", "t_x": 120},
     {"lamp": "Olli/Sony TV/Links/Hue Play", "t_x": 120},
-    {"lamp": "Olli/Sony TV/Rechts/Hue Play", "t_x": 120}
+    {"lamp": "Olli/Sony TV/Rechts/Hue Play", "t_x": 120},
 ]
 zone_waylight = [
     {"lamp": "Bibliothek/Hue white spot/R", "t_x": 120},
     {"lamp": "Bibliothek/Hue white spot/L", "t_x": 120},
     {"lamp": "Zwischenraum/Schrank/lightstrip", "t_x": 120},
-    {"lamp": "Flur/Haustür/Tisch/Hue white", "t_x": 120}
+    {"lamp": "Flur/Haustür/Tisch/Hue white", "t_x": 120},
 ]
 zone_nightmode = [
     {"lamp": "Bibliothek/Hue white spot/R", "t_x": 12},
     {"lamp": "Bibliothek/Hue white spot/L", "t_x": 12},
     {"lamp": "Zwischenraum/Schrank/lightstrip", "t_x": 12},
     {"lamp": "Flur/Haustür/Tisch/Hue white", "t_x": 12},
-    {"lamp": "Flur/Olli/Decke/", "t_x": 12}
+    {"lamp": "Flur/Olli/Decke/", "t_x": 12},
 ]
 
 
 lamp_states = {}
-for item in zone_daymode + zone_outside + zone_runway + zone_cominghome + zone_waylight + zone_nightmode + Olli:
-    lamp_states[item["lamp"]] = {"on": False, "last_turned_on_time": None, "brightness": None}
+for item in (
+    zone_daymode
+    + zone_outside
+    + zone_runway
+    + zone_cominghome
+    + zone_waylight
+    + zone_nightmode
+    + Olli
+):
+    lamp_states[item["lamp"]] = {
+        "on": False,
+        "last_turned_on_time": None,
+        "brightness": None,
+    }
 
-Pfad = '/home/oliver/Dokumente/autostart/outside.log'
+Pfad = "/home/oliver/Dokumente/autostart/outside.log"
 #'/home/oliver/Desktop/autostart/outside.log'
-#/home/olli/Schreibtisch/SAP/hue/daly/outside/outside.log
-logging.basicConfig(filename=Pfad, level=logging.INFO, format='%(asctime)s %(message)s')
-
+# /home/olli/Schreibtisch/SAP/hue/daly/outside/outside.log
+logging.basicConfig(filename=Pfad, level=logging.INFO, format="%(asctime)s %(message)s")
 
 
 def sunrise():
@@ -125,13 +136,13 @@ def sunrise():
 
     # Datum und Zeitzone für die Berechnung des Sonnenaufgangs
     date = datetime.date.today()
-    timezone = pytz.timezone('Europe/Berlin')
+    timezone = pytz.timezone("Europe/Berlin")
 
     # Berechnung der Uhrzeit für den Sonnenaufgang
     obs = ephem.Observer()
     obs.lat = str(latitude)
     obs.lon = str(longitude)
-    obs.date = date.strftime('%Y/%m/%d')
+    obs.date = date.strftime("%Y/%m/%d")
     sun = ephem.Sun()
     sunrise = obs.next_rising(sun)
 
@@ -139,43 +150,42 @@ def sunrise():
     sunrise = pytz.utc.localize(sunrise.datetime()).astimezone(timezone)
 
     # Formatierung des Sonnenaufgangs
-    formatted_sunrise_time = sunrise.strftime('%H:%M:%S')
+    formatted_sunrise_time = sunrise.strftime("%H:%M:%S")
     return formatted_sunrise_time
 
 
-
 def sunset():
-        # Geografische Koordinaten für den gewünschten Standort
-        latitude = 53.595283
-        longitude = 7.340012 
+    # Geografische Koordinaten für den gewünschten Standort
+    latitude = 53.595283
+    longitude = 7.340012
 
-        # Datum und Zeitzone für die Berechnung des Sonnenuntergangs
-        date = datetime.date.today()
-        timezone = pytz.timezone('Europe/Berlin')
+    # Datum und Zeitzone für die Berechnung des Sonnenuntergangs
+    date = datetime.date.today()
+    timezone = pytz.timezone("Europe/Berlin")
 
-        # Berechnung der Uhrzeit für den Sonnenuntergang
-        obs = ephem.Observer()
-        obs.lat = str(latitude)
-        obs.lon = str(longitude)
-        obs.date = date.strftime('%Y/%m/%d')
-        sun = ephem.Sun()
-        sunset = obs.next_setting(sun)
+    # Berechnung der Uhrzeit für den Sonnenuntergang
+    obs = ephem.Observer()
+    obs.lat = str(latitude)
+    obs.lon = str(longitude)
+    obs.date = date.strftime("%Y/%m/%d")
+    sun = ephem.Sun()
+    sunset = obs.next_setting(sun)
 
-        # Konvertierung der Sonnenuntergangszeit in die lokale Zeitzone
-        sunset = pytz.utc.localize(sunset.datetime()).astimezone(timezone)
+    # Konvertierung der Sonnenuntergangszeit in die lokale Zeitzone
+    sunset = pytz.utc.localize(sunset.datetime()).astimezone(timezone)
 
-        # Formatierung der Sonnenuntergangszeit
-        formatted_sunset_time = sunset.strftime('%H:%M:%S')
-        return formatted_sunset_time
+    # Formatierung der Sonnenuntergangszeit
+    formatted_sunset_time = sunset.strftime("%H:%M:%S")
+    return formatted_sunset_time
 
 
 def check_lamp_state(lamp_array):
     current_datetime = datetime.datetime.now()
-    
+
     true_lamps = []
     for item in lamp_array:
         lamp = item["lamp"]
-        on_state = b.get_light(lamp, 'on')
+        on_state = b.get_light(lamp, "on")
 
         if on_state:
             true_lamps.append(lamp)
@@ -192,14 +202,13 @@ def check_lamp_state(lamp_array):
         if t_x:
             t_x = t_x[0]
             if (current_datetime - last_turned_on_time).total_seconds() > t_x:
-                b.set_light(lamp, 'on', False)
+                b.set_light(lamp, "on", False)
                 lamp_states[lamp]["on"] = False
                 lamp_states[lamp]["last_turned_on_time"] = None
-    #return true_lamps
+    # return true_lamps
 
 
 def main_function():
-    
     logging.info("\nRestart")
 
     Morning = 0
@@ -208,68 +217,71 @@ def main_function():
     Night = 0
 
     while True:
-
         formatted_sunrise_time = sunrise()
         formatted_sunset_time = sunset()
         deltatime = datetime.timedelta(hours=1)
 
-        sunrise_time = datetime.datetime.strptime(formatted_sunrise_time, "%H:%M:%S").time()
-        sunrise_time_deltatime = datetime.datetime.combine(datetime.datetime.today(), sunrise_time) - deltatime
+        sunrise_time = datetime.datetime.strptime(
+            formatted_sunrise_time, "%H:%M:%S"
+        ).time()
+        sunrise_time_deltatime = (
+            datetime.datetime.combine(datetime.datetime.today(), sunrise_time)
+            - deltatime
+        )
         sunrise_time_deltatime = sunrise_time_deltatime.time()
-        
-        sunset_time = datetime.datetime.strptime(formatted_sunset_time, "%H:%M:%S").time()
-        sunset_time_deltatime = datetime.datetime.combine(datetime.datetime.today(), sunset_time) + deltatime
+
+        sunset_time = datetime.datetime.strptime(
+            formatted_sunset_time, "%H:%M:%S"
+        ).time()
+        sunset_time_deltatime = (
+            datetime.datetime.combine(datetime.datetime.today(), sunset_time)
+            + deltatime
+        )
         sunset_time_deltatime = sunset_time_deltatime.time()
 
-
         if sunrise_time < datetime.datetime.now().time() < sunset_time:
-
             if Day != 1:
-                Day +=1
+                Day += 1
                 Night = 0
-                logging.info("Day - Mode") 
+                logging.info("Day - Mode")
 
             check_lamp_state(zone_daymode)
-            
 
         else:
-
-
             if sunset_time <= datetime.datetime.now().time() < sunset_time_deltatime:
-
                 if Afternoon != 1:
-                    Afternoon +=1
+                    Afternoon += 1
                     Day = 0
                     logging.info("Evening - Mode")
-                
-                b.set_light(zone_waylight,'on', True)
-                b.set_light(zone_outside,'on', True)
-                b.set_light(zone_runway, 'on', True)
 
-                b.set_light(zone_outside,'bri', 60)
-                b.set_light(zone_waylight, 'bri', 50)
-                b.set_light(zone_runway, 'bri', 20)
+                b.set_light(zone_waylight, "on", True)
+                b.set_light(zone_outside, "on", True)
+                b.set_light(zone_runway, "on", True)
 
-            elif sunrise_time_deltatime < datetime.datetime.now().time() <= sunrise_time:
-                
+                b.set_light(zone_outside, "bri", 60)
+                b.set_light(zone_waylight, "bri", 50)
+                b.set_light(zone_runway, "bri", 20)
+
+            elif (
+                sunrise_time_deltatime < datetime.datetime.now().time() <= sunrise_time
+            ):
                 if Morning != 1:
-                    Morning +=1
+                    Morning += 1
                     Afternoon = 0
                     logging.info("Morning - Mode")
 
                 for lamp in zone_waylight:
-                    b.set_light(lamp,'on', True)
-                    b.set_light(lamp, 'bri', 60)
+                    b.set_light(lamp, "on", True)
+                    b.set_light(lamp, "bri", 60)
                 for lamp in zone_outside:
-                    b.set_light(lamp,'on', True)
-                    b.set_light(lamp, 'bri', 50)
+                    b.set_light(lamp, "on", True)
+                    b.set_light(lamp, "bri", 50)
                 for lamp in zone_runway:
-                    b.set_light(lamp,'on', True)
-                    b.set_light(lamp, 'bri', 20)
+                    b.set_light(lamp, "on", True)
+                    b.set_light(lamp, "bri", 20)
             else:
-                
                 if Night != 1:
-                    Night +=1
+                    Night += 1
                     Morning = 0
                     logging.info("Night - Mode")
 
@@ -277,15 +289,13 @@ def main_function():
                 check_lamp_state(zone_runway)
 
                 for lamp in zone_waylight:
-                    b.set_light(lamp,'on', True)
-                    b.set_light(lamp, 'bri', 20)
-                
+                    b.set_light(lamp, "on", True)
+                    b.set_light(lamp, "bri", 20)
 
-            on = b.get_light(Dummy[0], 'on')
-            b.set_light(Dummy[0], 'on', False)
+            on = b.get_light(Dummy[0], "on")
+            b.set_light(Dummy[0], "on", False)
 
             if on == True:
-                
                 logging.info("Comminghome - Mode")
                 time.sleep(18)
                 for light in zone_runway:
@@ -293,9 +303,9 @@ def main_function():
                     light_names[light].brightness = 250
                     time.sleep(1)
                 time.sleep(10)
-                b.set_light(zone_cominghome,'on', True)
-                b.set_light(zone_cominghome,'bri', 180)
-                
+                b.set_light(zone_cominghome, "on", True)
+                b.set_light(zone_cominghome, "bri", 180)
+
                 time.sleep(600)
 
 
